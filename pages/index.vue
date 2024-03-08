322d4ce62f0f5f866653e73ce89eb731952f588f
Ampoi <tange.toshihiro@outlook.jp>
2024-03-08 11:10:22 +0900

    スワイプできるようにした

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,32 @@
 <template>
-    <main class="w-screen h-screen bg-black relative">
-        <div
-            class="absolute w-full h-full top-0 left-0"
-            @click="isWriting = !isWriting">
+    <Swiper
+        :loop="true"
+        :slides-per-view="1"
+        @slide-change-transition-end="isWriting = false"
+        class="w-screen h-screen bg-black">
+        <SwiperSlide class="w-full h-full relative">
             <div
-                class="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 text-white font-serif text-5xl text-center"
-                v-if="!isWriting">
-                Drop your piece of imagination
+                class="absolute w-full h-full top-0 left-0"
+                @click="isWriting = !isWriting">
+                <div
+                    class="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 text-white font-serif text-5xl text-center"
+                    v-if="!isWriting">
+                    Drop your piece of imagination
+                </div>
             </div>
-        </div>
-        <div
-            v-if="isWriting"
-            class="absolute w-full top-20 left-0 p-8 flex flex-col gap-4">
-            <textarea
-                v-model="note"
-                class="text-center outline-none bg-transparent text-white basis-20"/>
-            <button class="text-white bg-gradient-to-b from-white/15 to-white/10 p-2 border-white/10 border-[1px] rounded-md via-30%">Submit</button>
-        </div>
-    </main>
+            <div
+                v-if="isWriting"
+                class="absolute w-full top-20 left-0 p-8 flex flex-col gap-4">
+                <textarea
+                    v-model="note"
+                    class="text-center outline-none bg-transparent text-white basis-20"/>
+                <button class="text-white bg-gradient-to-b from-white/15 to-white/10 p-2 border-white/10 border-[1px] rounded-md via-30%">Submit</button>
+            </div>
+        </SwiperSlide>
+        <SwiperSlide>
+            vvv
+        </SwiperSlide>
+    </Swiper>
 </template>
 <script setup lang="ts">
 const isWriting = ref(false)
