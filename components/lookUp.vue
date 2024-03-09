19694d577d91a28774e1d48e55beca6fd0eabce1
Ampoi <tange.toshihiro@outlook.jp>
2024-03-09 12:42:30 +0900

    メモの星の拡大縮小や移動ができるようになった

--- components/lookUp.vue
+++ components/lookUp.vue
@@ -5,11 +5,14 @@
             '-translate-y-[calc(100dvh-10rem)]': !isLookingUp
         }">
         <div
-            class="h-[calc(100dvh-10rem)] w-full relative"
-            ref="movableArea">
+            class="h-[calc(100dvh-10rem)] w-full relative overflow-hidden"
+            ref="movableArea"
+            @touchmove="onTouchMove"
+            @touchend="endTouching"
+            @touchcancel="endTouching">
             <div
                 v-for="star in noteStars"
-                class="size-2 rounded-full absolute bg-white -translate-x-1/2 -translate-y-1/2"
+                class="size-0.5 rounded-full absolute bg-white -translate-x-1/2 -translate-y-1/2"
                 :style="{
                     left: `calc(${(star.x - starCenter.x) * magnificationRate}px + 50%)`,
                     top:  `calc(${(star.y - starCenter.y) * magnificationRate}px + 50%)`,
@@ -33,11 +36,7 @@ const noteStars = ref<{
     id: string | number;
     x: number;
     y: number;
-}[]>([
-    {id: "aa", x: 3, y: 5},
-    {id: "aa", x: 1, y: 4},
-    {id: "aa", x: 2, y: 3}
-])
+}[]>([])
 const starCenter = reactive({x: 0, y: 0})
 const magnificationRate = ref(1)
 const movableArea = ref<HTMLElement>()
@@ -46,7 +45,7 @@ const movableAreaPadding = 80
 const starSize = computed(() => {
     const size = magnificationRate.value ** 0.5
     const min = 1
-    const max = 2
+    const max = 10
     return {
         size,
         showSize: size < min ? min : max < size ? max : size
@@ -55,7 +54,7 @@ const starSize = computed(() => {
 
 watch(isLookingUp, async (newValue) => {
     if( newValue ){
-        //noteStars.value = await getAllNoteStars()
+        noteStars.value = await getAllNoteStars()
         if( noteStars.value.length == 0 ) return
         
         const bound = {
@@ -92,4 +91,72 @@ watch(isLookingUp, async (newValue) => {
         )
     }
 })
+
+const touches = ref<TouchList>()
+
+function findTouch(identifier: number, touches: TouchList){
+    let touch: Touch | undefined = undefined
+    for( let j = 0; j < touches.length; j++ ){
+        if(touches[j].identifier == identifier){
+            touch = touches[j]
+            break
+        }
+    }
+    return touch
+}
+
+function onTouchMove(event: TouchEvent){
+    event.preventDefault()
+    const newTouches = event.touches
+    if( touches.value ){
+        const toucheMovements: Record<
+            "previous" | "following",
+            Record<"x" | "y", number>
+        >[] = []
+
+        for( let i = 0; i < newTouches.length; i++){
+            const newTouch = newTouches[i]
+            const touch = findTouch(newTouch.identifier, touches.value)
+            if( !touch ) continue
+            const previous = {
+                x: touch.clientX,
+                y: touch.clientY
+            }
+            const following = {
+                x: newTouch.clientX,
+                y: newTouch.clientY
+            }
+            toucheMovements.push({ previous, following })
+        }
+
+        if( toucheMovements.length == 1 ){
+            starCenter.x -= (toucheMovements[0].following.x - toucheMovements[0].previous.x) / magnificationRate.value
+            starCenter.y -= (toucheMovements[0].following.y - toucheMovements[0].previous.y) / magnificationRate.value
+        }else if( toucheMovements.length == 2 ){
+            const previousLength = Math.sqrt(
+                (toucheMovements[1].previous.x - toucheMovements[0].previous.x) ** 2 +
+                (toucheMovements[1].previous.y - toucheMovements[0].previous.y) ** 2
+            )
+            const followingLength = Math.sqrt(
+                (toucheMovements[1].following.x - toucheMovements[0].following.x) ** 2 +
+                (toucheMovements[1].following.y - toucheMovements[0].following.y) ** 2
+            )
+            const newMagnificationRate = magnificationRate.value * followingLength / previousLength
+            const magnificationRateMax = 400
+            const magnificationRateMin = 0.5
+            magnificationRate.value =
+                newMagnificationRate < magnificationRateMin ?
+                    magnificationRateMin :
+                    magnificationRateMax < newMagnificationRate ?
+                        magnificationRateMax :
+                        newMagnificationRate
+            console.log(magnificationRate.value)
+        }
+    }
+    touches.value = newTouches
+}
+
+function endTouching(){
+    touches.value = undefined
+}
 </script>
